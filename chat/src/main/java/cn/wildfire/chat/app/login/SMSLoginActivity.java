db7399800507272f498e9e934fe614a60e243166
ShanZhongSanXian <870491041@qq.com>
2019-05-28 09:56:54 +0800

    登录页面绘制，

--- chat/src/main/java/cn/wildfire/chat/app/login/SMSLoginActivity.java
+++ chat/src/main/java/cn/wildfire/chat/app/login/SMSLoginActivity.java
@@ -1,34 +1,129 @@
 package cn.wildfire.chat.app.login;
 
-import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.OnClick;
 import butterknife.OnTextChanged;
+import cn.wildfire.chat.app.Config;
 import cn.wildfire.chat.kit.WfcBaseActivity;
+import cn.wildfire.chat.kit.net.OKHttpHelper;
+import cn.wildfire.chat.kit.net.SimpleCallback;
+import cn.wildfire.chat.kit.net.base.StatusResult;
 import cn.wildfirechat.chat.R;
 
 public class SMSLoginActivity extends WfcBaseActivity {
-    @Bind(R.id.loginButton)
-    Button loginButton;
-    @Bind(R.id.phoneNumberEditText)
-    EditText phoneNumberEditText;
+
+    @Bind(R.id.phone_number_editText)
+    private EditText phoneNumberEditText;
+
+    @Bind(R.id.verify_code_editText)
+    private EditText verifyCodeEditText;
+
+    @Bind(R.id.get_verify_code)
+    private TextView getVerifyCode;
+
+    @Bind(R.id.btn_login)
+    private Button loginButton;
+
+    private Handler handler = new Handler();
 
     @Override
-    protected int contentLayout() {
-        return R.layout.login_activity_sms;
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStatusBarFullTransparent();
     }
 
-    @OnTextChanged(value = R.id.phoneNumberEditText, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    //监听手机号的变化
+    @OnTextChanged(value = R.id.phone_number_editText, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
     void inputPhoneNumber(Editable editable) {
         String phone = editable.toString().trim();
         if (phone.length() == 11) {
-            loginButton.setEnabled(true);
+
+            loginButton.setClickable(true);
+            getVerifyCode.setClickable(true);
         } else {
-            loginButton.setEnabled(false);
+
+            loginButton.setClickable(false);
+            getVerifyCode.setClickable(false);
+        }
+    }
+
+    @OnClick(R.id.get_verify_code)
+    void getVerifyCode() {
+
+        getVerifyCode.setEnabled(false);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!isFinishing()) {
+                    getVerifyCode.setEnabled(true);
+                }
+            }
+        }, 60 * 1000);
+
+        String url = "http://" + Config.APP_SERVER_HOST + ":" + Config.APP_SERVER_PORT + "/send_code";
+        Map<String, String> params = new HashMap<>();
+        params.put("mobile", phoneNumberEditText.getText().toString());
+        OKHttpHelper.post(url, params, new SimpleCallback<StatusResult>() {
+            @Override
+            public void onUiSuccess(StatusResult statusResult) {
+                if (statusResult.getCode() == 0) {
+                    Toast.makeText(SMSLoginActivity.this, "发送验证码成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SMSLoginActivity.this, "发送验证码失败: " + statusResult.getCode(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onUiFailure(int code, String msg) {
+                Toast.makeText(SMSLoginActivity.this, "发送验证码失败: " + msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @OnClick(R.id.btn_login)
+    void login() {
+        String phoneNumber = phoneNumberEditText.getText().toString().trim();
+    }
+
+    /**
+     * 设置状态栏透明同时设置虚拟按钮隐藏
+     */
+    protected void setStatusBarFullTransparent() {
+
+        //5.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
     }
 
@@ -37,11 +132,8 @@ public class SMSLoginActivity extends WfcBaseActivity {
         return false;
     }
 
-    @OnClick(R.id.loginButton)
-    void login() {
-        String phoneNumber = phoneNumberEditText.getText().toString().trim();
-        Intent intent = new Intent(SMSLoginActivity.this, AuthCodeActivity.class);
-        intent.putExtra("phoneNumber", phoneNumber);
-        startActivity(intent);
+    @Override
+    protected int contentLayout() {
+        return R.layout.login_activity_sms;
     }
 }
