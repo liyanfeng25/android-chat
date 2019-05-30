0ad9b448a16eaaa9cee6f240554580d05c15d46b
liyanfeng <15010970432@163.com>
2019-05-30 23:25:46 +0800

    我的页面简单布局1

--- chat/src/main/java/cn/wildfire/chat/kit/WfcBaseActivity.java
+++ chat/src/main/java/cn/wildfire/chat/kit/WfcBaseActivity.java
@@ -54,7 +54,6 @@ public abstract class WfcBaseActivity extends AppCompatActivity {
             window.getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_IMMERSIVE);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
